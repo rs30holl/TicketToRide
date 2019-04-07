4b09354ab33f88107516670862884bebf53a4909
matt-f-harrison <matt-f-harrison@live.com>
2019-04-07 14:29:23 -0400

    added destination tickets

--- Board.java
+++ Board.java
@@ -1,26 +1,19 @@
-import java.util.*; //imports necessary to implement this class.
-import java.awt.Color; 
+import java.util.*;
+import java.awt.Color;
 /**
- * Class designed to construct the board in its entirety. This includes the
- * locations, paths, card deck(s), and discarded card deck(s)
- * @author Ryan Holland, Julia Krasinski, Briella Sala, Matt Harrison,          
- * & Michael Lostritto
- * @version (04.07.2019)
+ * Write a description of class Board here.
+ *
+ * @author Ryan Holland, Julia Krasinski, Briella Sala,
+ * Matt Harrison, Michael Lostritto
+ * @version (a version number or a date)
  */
 public class Board
 {
-    //list of points that exist on the board
     private ArrayList<LocationNode> points = new ArrayList<>();
-    //list of cards which emulates a card deck 
-    public static ArrayList<Card> deck = new ArrayList<>(); 
-    //list of cards that have been used
+    public static ArrayList<Card> tcDeck = new ArrayList<>();
+    public static ArrayList<Card> dtDeck = new ArrayList<>();    
     public static ArrayList<Card> discard = new ArrayList<>();
-    /**
-     * Empty Constructor which constructs the board with all locations, and 
-     * given paths to each location. The deck is also set up with the 6 
-     * transportation cards with their respective colors using the initDeck
-     * method.
-     */
+
     public Board(){
         LocationNode cp = new LocationNode("Central Park", 0, 0, true);
         points.add(cp);
@@ -76,12 +69,12 @@ public class Board
         mw.addPath(new Path(mw, esb, "GREEN", 0, 2));
         mw.addPath(new Path(mw, ch, "BLUE", 0, 2));
         mw.addPath(new Path(mw, ts, "GRAY", 0, 1));
-
+        
         un.addPath(new Path(un, cp, "PINK", 0, 3));
         un.addPath(new Path(un, ts, "GRAY", 0, 2));
         un.addPath(new Path(un, esb, "BLACK", 0, 2));
         un.addPath(new Path(un, gp, "GREEN", 0, 3));
-
+        
         esb.addPath(new Path(esb, ts, "PINK", 0, 1));
         esb.addPath(new Path(esb, ts, "ORANGE", 0, 1));
         esb.addPath(new Path(esb, un, "BLACK", 0, 2));
@@ -90,7 +83,7 @@ public class Board
         esb.addPath(new Path(esb, ch, "GRAY", 0, 2));
         esb.addPath(new Path(esb, ch, "GRAY", 0, 2));
         esb.addPath(new Path(esb, mw, "GREEN", 0, 2));
-
+        
         ch.addPath(new Path(ch, mw, "BLUE", 0, 2));
         ch.addPath(new Path(ch, so, "PINK", 0, 4));
         ch.addPath(new Path(ch, gv, "RED", 0, 3));
@@ -98,7 +91,7 @@ public class Board
         ch.addPath(new Path(ch, gp, "ORANGE", 0, 2));
         ch.addPath(new Path(ch, esb, "GRAY", 0, 2));
         ch.addPath(new Path(ch, esb, "GRAY", 0, 2));
-
+        
         gp.addPath(new Path(gp, un, "GREEN", 0, 3));
         gp.addPath(new Path(gp, ev, "GRAY", 0, 2));
         gp.addPath(new Path(gp, gv, "PINK", 0, 2));
@@ -106,7 +99,7 @@ public class Board
         gp.addPath(new Path(gp, esb, "BLUE", 0, 1));
         gp.addPath(new Path(gp, esb, "RED", 0, 1));
         gp.addPath(new Path(gp, ch, "ORANGE", 0, 2));
-
+        
         gv.addPath(new Path(gv, ev, "BLUE", 0, 2));
         gv.addPath(new Path(gv, so, "ORANGE", 0, 2));
         gv.addPath(new Path(gv, les, "GRAY", 0, 2));
@@ -116,15 +109,15 @@ public class Board
         gv.addPath(new Path(gv, gp, "BLACK", 0, 2));
         gv.addPath(new Path(gv, ch, "RED", 0, 3));
         gv.addPath(new Path(gv, ch, "GREEN", 0, 3));
-
+        
         ev.addPath(new Path(ev, gp, "GRAY", 0, 2));
         ev.addPath(new Path(ev, gv, "BLUE", 0, 2));
         ev.addPath(new Path(ev, les, "BLACK", 0, 1));
-
+        
         so.addPath(new Path(so, ch, "PINK", 0, 4));
         so.addPath(new Path(so, gv, "ORANGE", 0, 2));
         so.addPath(new Path(so, ws, "GRAY", 0, 2));
-
+        
         ct.addPath(new Path(ct, gv, "GRAY", 0, 2));
         ct.addPath(new Path(ct, gv, "GRAY", 0, 2));
         ct.addPath(new Path(ct, les, "BLUE", 0, 1));
@@ -132,32 +125,48 @@ public class Board
         ct.addPath(new Path(ct, bk, "ORANGE", 0, 3));
         ct.addPath(new Path(ct, ws, "PINK", 0, 1));
         ct.addPath(new Path(ct, ws, "GREEN", 0, 1));
-
+        
         les.addPath(new Path(les, ev, "BLACK", 0, 1));
         les.addPath(new Path(les, ct, "BLUE", 0,1));
         les.addPath(new Path(les, gv, "GRAY", 0, 2));
         les.addPath(new Path(les, bk, "GRAY", 0, 3));
-
+        
         ws.addPath(new Path(ws, ct, "GREEN", 0, 1));
         ws.addPath(new Path(ws, ct, "PINK", 0, 1));
         ws.addPath(new Path(ws, so, "GRAY", 0, 2));
         ws.addPath(new Path(ws, bk, "BLUE", 0, 3));
         ws.addPath(new Path(ws, bk, "BLACK", 0, 3));
-
+        
         bk.addPath(new Path(bk, ws, "BLUE", 0, 3));
         bk.addPath(new Path(bk, ws, "BLACK", 0, 3));
         bk.addPath(new Path(bk, ct, "ORANGE", 0, 3));
         bk.addPath(new Path(bk, ct, "RED", 0, 3));
         bk.addPath(new Path(bk, les, "GRAY", 0, 3));
-
+        
+        //Initialize the destination tickets
+        dtDeck.add(new DestinationTicket(un,ws,8));
+        dtDeck.add(new DestinationTicket(lc,gv,6));
+        dtDeck.add(new DestinationTicket(esb,bk,7));
+        dtDeck.add(new DestinationTicket(cp,ct,8));
+        dtDeck.add(new DestinationTicket(ts,so,6));
+        dtDeck.add(new DestinationTicket(ch,ws,6));
+        dtDeck.add(new DestinationTicket(cp,ch,5));
+        dtDeck.add(new DestinationTicket(ts,ev,4));
+        dtDeck.add(new DestinationTicket(ev,so,4));
+        dtDeck.add(new DestinationTicket(ch,bk,8));
+        dtDeck.add(new DestinationTicket(cp,gp,4));
+        dtDeck.add(new DestinationTicket(gp,ct,4));
+        dtDeck.add(new DestinationTicket(un,mw,3));
+        dtDeck.add(new DestinationTicket(cp,mw,2));
+        dtDeck.add(new DestinationTicket(lc,esb,3));
+        dtDeck.add(new DestinationTicket(les,ws,2));
+        dtDeck.add(new DestinationTicket(esb,gv,3));
+        dtDeck.add(new DestinationTicket(ts,bk,8));
+        Collections.shuffle(dtDeck);
+        
         initDeck();
     }
-
-    /**
-     * Method which creates the deck of transportation cards by adding them to 
-     * the deck arrayList. The deck is then shuffled to ensure random placement
-     * of all cards (including rainbow)
-     */
+    
     public void initDeck(){
         for (int i = 0; i < 6; i++){
             TransportationCard a = new TransportationCard("BLUE");
@@ -166,26 +175,22 @@ public class Board
             TransportationCard d = new TransportationCard("ORANGE");
             TransportationCard e = new TransportationCard("PINK");
             TransportationCard f = new TransportationCard("GREEN");
-            deck.add(a);
-            deck.add(b);
-            deck.add(c);
-            deck.add(d);
-            deck.add(e);
-            deck.add(f);
+            tcDeck.add(a);
+            tcDeck.add(b);
+            tcDeck.add(c);
+            tcDeck.add(d);
+            tcDeck.add(e);
+            tcDeck.add(f);
         }
-
+        
         for (int i = 0; i < 8; i++){
             TransportationCard c = new TransportationCard("RAINBOW");
-            deck.add(c);
+            tcDeck.add(c);
         }
-
-        Collections.shuffle(deck);
+        
+        Collections.shuffle(tcDeck);
     }
-
-    /**
-     * Main program
-     * @param args command line arguments (not used)
-     */
+    
     public static void main(String[] args){
         Board b = new Board();
         for (LocationNode n : b.points){
