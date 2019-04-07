8d2ea77efdc1fca80ed16928b107f7660cda39ee
Ryan Holland <43013147+rs30holl@users.noreply.github.com>
2019-04-07 14:22:01 -0400

    Update OpeningScreen.java

--- OpeningScreen.java
+++ OpeningScreen.java
@@ -10,7 +10,7 @@ import java.awt.event.*;
  */
 public class OpeningScreen extends JPanel
 {
-    private final ImageIcon cover = new ImageIcon(this.getClass().getResource("/fwdpieces/Cover.jpg"));
+    private final ImageIcon cover = new ImageIcon(this.getClass().getResource("/fwdpieces1/board.jpg"));
     private final Image cover2 = cover.getImage();
 
     public OpeningScreen(){
