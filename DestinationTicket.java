485bbd29de7a91f7acab2b2943d744b514655b60
ba28sala <46751387+ba28sala@users.noreply.github.com>
2019-04-07 13:41:57 -0400

    Add files via upload
    Comments for the DestinationTicket class

--- DestinationTicket.java
+++ DestinationTicket.java
@@ -1,30 +1,47 @@
 import java.awt.Color;
 /**
- * Write a description of class DestinationTicket here.
+ * This is a class for the destination cards.
  *
  * @author Ryan Holland, Julia Krasinski, Briella Sala,
  * Matt Harrison, Michael Lostritto
- * @version (a version number or a date)
+ * @version (4/7/2019)
  */
 public class DestinationTicket extends Card
 {
     private LocationNode start, end;
     private int score;
     
+    /**
+     * Constructor for the objects of DestinationCard class.
+     * 
+     * @param s: LocationNode
+     * @param e: LocationNode
+     * @param p: int of score
+     */
     public DestinationTicket(LocationNode s, LocationNode e, int p){
         start = s;
         end = e;
         score = p;
     }
     
+    
+    /**
+     * Returns the starting location.
+     */
     public LocationNode getStart(){
         return start;
     }
     
+    /**
+     * Returns the ending location.
+     */
     public LocationNode getEnd(){
         return end;
     }
     
+    /**
+     * Gets the color of the card. 
+     */
     @Override
     public String getColor(){return null;}
 }
