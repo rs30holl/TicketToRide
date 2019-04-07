aa580cb719825a0cee8b01506d9c8d9f1c9d8139
matt-f-harrison <matt-f-harrison@live.com>
2019-04-07 13:33:11 -0400

    added comments

--- Hand.java
+++ Hand.java
@@ -1,25 +1,40 @@
 import java.util.*;
 /**
- * Write a description of class Hand here.
+ * Creates a hand of Ticket to Ride cards
  *
  * @author Ryan Holland, Julia Krasinski, Briella Sala,
  * Matt Harrison, Michael Lostritto
- * @version (a version number or a date)
+ * @version 4/7/19
  */
 public class Hand
 {
     public ArrayList<Card> cards = new ArrayList<>();
     
+    /**
+     * Constructor for a hand object.
+     *
+     * @param numCards Number of starting cards in a hand
+     */
     public Hand(Card... numCards){
         for (Card c : numCards){
             cards.add(c);
         }
     }
     
+    /**
+     * Adds a card to the hand
+     *
+     * @param c The card to be added to the hand
+     */
     public void addCard(Card c){
         cards.add(c);
     }
     
+    /**
+     * Removes a card from the hand
+     *
+     * @param c The card to be removed
+     */
     public void removeCard(Card c){
         cards.remove(c);
     }
