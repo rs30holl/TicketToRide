361d1dbf811f181b769bae01fe00234c5f458896
rs30holl <rs30holl@siena.edu>
2019-04-16 12:10:41 -0400

    Added extra images

--- BoardPanel.java
+++ BoardPanel.java
@@ -11,19 +11,42 @@ public class BoardPanel extends JPanel
 {
     private final ImageIcon board = new ImageIcon(this.getClass().getResource("BoardPicture.jpg"));
     private final Image board2 = board.getImage();
-    
+    private  final ImageIcon trainTracks = new ImageIcon(this.getClass().getResource("tracks.jpg"));
+    private final Image trainTracks2 = trainTracks.getImage();
+    private  final ImageIcon table = new ImageIcon(this.getClass().getResource("table.jpg"));
+    private final Image table2 = table.getImage();
+    private final ImageIcon transCardBack = new ImageIcon(this.getClass().getResource(".\\fwdpieces1\\verticalCardFace.jpg"));
+    private final Image transCardBack2 = transCardBack.getImage();
+    private final ImageIcon destTicketBack = new ImageIcon(this.getClass().getResource("\\fwdboardandtransport1\\frontOfCard.jpg"));
+    private final Image destTicketBack2 = destTicketBack.getImage();
+
+    /**
+     *
+     */
     public BoardPanel(){
-        setPreferredSize(new Dimension(1000,1500));
-        
+        setPreferredSize(new Dimension(2500,1500));
     }
-    
+
+    /**
+     * @param g
+     */
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        int halfWidth = (int)(0.5 * getWidth());
+        int quarterHeight = (int)(0.75 * getHeight());
         
-        g.drawImage(board2, 0, 0, getWidth(), getHeight(), this);
+        g.drawImage(board2, 0, 0, halfWidth, getHeight(), this);
+        g.drawImage(trainTracks2, halfWidth, 0, halfWidth, quarterHeight,this);
+        g.drawImage(table2, halfWidth, quarterHeight, halfWidth, quarterHeight / 3, this);
+        g.drawImage(transCardBack2, (int)(0.75 * getWidth()), getHeight() % 100, getWidth() / 10, getHeight() % 100, this);
+        g.drawImage(destTicketBack2, 0, 0, getWidth() / 10, getHeight() % 100, this);
+        g.drawImage(transCardBack2, 0, 0, getWidth() / 10, getHeight() % 100, this);
     }
 
+    /**
+     *
+     */
     public static void createAndShowGUI(){
         JFrame frame = new JFrame("Ticket to Ride: NYC");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,6 +58,9 @@ public class BoardPanel extends JPanel
         frame.setVisible(true);
     }
 
+    /**
+     * @param args
+     */
     public static void main(String[] args){
         javax.swing.SwingUtilities.invokeLater(new Runnable(){
                 public void run(){
--- OpeningScreen.java
+++ OpeningScreen.java
@@ -12,6 +12,7 @@ public class OpeningScreen extends JPanel implements MouseListener
 {
     private final ImageIcon cover = new ImageIcon(this.getClass().getResource("/fwdpieces1/Cover.jpg"));
     private final Image cover2 = cover.getImage();
+    private static final JFrame frame = new JFrame("Ticket to Ride: NYC");
 
     public OpeningScreen(){
         setPreferredSize(new Dimension(1000,1000));
@@ -26,7 +27,7 @@ public class OpeningScreen extends JPanel implements MouseListener
     }
 
     public static void createAndShowGUI(){
-        JFrame frame = new JFrame("Ticket to Ride: NYC");
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         OpeningScreen panel = new OpeningScreen();
@@ -46,6 +47,7 @@ public class OpeningScreen extends JPanel implements MouseListener
     
     public void mouseClicked(MouseEvent e){
         BoardPanel.main(new String[0]);
+        frame.dispose();
     }
 
     public static void main(String[] args){
