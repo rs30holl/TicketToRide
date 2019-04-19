8e7363beabceecbabc0a051f281be9308589176b
Julia Krasinski <43008316+jl08kras@users.noreply.github.com>
2019-04-19 12:23:35 -0400

    Add files via upload
    Edited location of decks

--- BoardPanel.java
+++ BoardPanel.java
@@ -44,8 +44,8 @@ public class BoardPanel extends JPanel
         //g.drawImage(transCardBack2, (int)(0.75 * getWidth()), getHeight() % 100, getWidth() / 10, getHeight() % 100, this);
         //g.drawImage(destTicketBack2, 0, 0, getWidth() / 10, getHeight() % 100, this);
         //g.drawImage(transCardBack2, 0, 0, getWidth() / 10, getHeight() % 100, this);
-        g.drawImage(transCardBack2, (int)(0.71 * getWidth()), 40, getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(destTicketBack2, (int)(0.71 * getWidth()), 290, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.715 * getWidth()), 40, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(destTicketBack2, (int)(0.715 * getWidth()), 300, getWidth() / 15, getHeight() / 5, this);
         g.drawImage(transCardSide2, (int)(0.865 * getWidth()), 40, getWidth() / 10, getHeight() / 10, this);
     }
 
