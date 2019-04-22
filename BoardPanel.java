bfc531c6b795529bfa9138ac9f31b2e875b0562b
Julia Krasinski <43008316+jl08kras@users.noreply.github.com>
2019-04-22 16:14:41 -0400

    updates to format

--- BoardPanel.java
+++ BoardPanel.java
@@ -38,7 +38,7 @@ public class BoardPanel extends JPanel implements MouseListener
             getResource("\\fwdboardandtransport1\\frontOfCard.jpg"));
 
     private final Image destTicketBack2 = destTicketBack.getImage();
-    private static ArrayList<Player> list;
+    private static ArrayList<Player> list = new ArrayList<Player>();
 
     /**
      *
@@ -62,18 +62,23 @@ public class BoardPanel extends JPanel implements MouseListener
         g.drawImage(trainTracks2, halfWidth, 0, halfWidth, quarterHeight,this);
         g.drawImage(table2, halfWidth, quarterHeight, halfWidth, 
             quarterHeight / 3, this);
-        //g.drawImage(transCardBack2, (int)(0.75 * getWidth()), getHeight() % 100, getWidth() / 10, getHeight() % 100, this);
-        //g.drawImage(destTicketBack2, 0, 0, getWidth() / 10, getHeight() % 100, this);
-        //g.drawImage(transCardBack2, 0, 0, getWidth() / 10, getHeight() % 100, this);
-        g.drawImage(transCardBack2, (int)(0.715 * getWidth()), 40, getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(destTicketBack2, (int)(0.715 * getWidth()), 270, getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardSide2, (int)(0.865 * getWidth()), 40, getWidth() / 10, getHeight() / 8, this);
+        g.drawImage(transCardBack2, (int)(0.715 * getWidth()), 
+            40, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(destTicketBack2, (int)(0.715 * getWidth()), 
+            270, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardSide2, (int)(0.865 * getWidth()), 
+            40, getWidth() / 10, getHeight() / 8, this);
 
-        g.drawImage(transCardBack2, (int)(0.51 * getWidth()), 750, getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardBack2, (int)(0.6 * getWidth()), 750, getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardBack2, (int)(0.7 * getWidth()), 750, getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardBack2, (int)(0.8 * getWidth()), 750, getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardBack2, (int)(0.9 * getWidth()), 750, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.51 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.6 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.7 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.8 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.9 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
         g.drawImage(transCardBack2, (int)(0.715 * getWidth()), 
             40, getWidth() / 15, getHeight() / 5, this);
         g.drawImage(destTicketBack2, (int)(0.715 * getWidth()), 
@@ -81,22 +86,22 @@ public class BoardPanel extends JPanel implements MouseListener
         g.drawImage(transCardSide2, (int)(0.865 * getWidth()), 40, 
             getWidth() / 10, getHeight() / 8, this);
 
-        g.drawImage(transCardBack2, (int)(0.51 * getWidth()), 750, 
-            getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardBack2, (int)(0.6 * getWidth()), 750, 
-            getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardBack2, (int)(0.7 * getWidth()), 750, 
-            getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardBack2, (int)(0.8 * getWidth()), 750, 
-            getWidth() / 15, getHeight() / 5, this);
-        g.drawImage(transCardBack2, (int)(0.9 * getWidth()), 750, 
-            getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.51 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.6 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.7 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.8 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
+        g.drawImage(transCardBack2, (int)(0.9 * getWidth()), 
+            quarterHeight +25, getWidth() / 15, getHeight() / 5, this);
     }
 
     /**
      *
      */
-    public static void createAndShowGUI(){
+    public static void createAndShowGUI(){ 
         //JFrame frame = new JFrame("Ticket to Ride: NYC");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -125,14 +130,16 @@ public class BoardPanel extends JPanel implements MouseListener
             Player p2 = new Player(name2, a2);
 
             if(p1.getAge() < p2.getAge()){
+                JOptionPane.showMessageDialog(frame, p1.getName() + " goes first.");
                 list.add(p1);
                 list.add(p2);
-                JOptionPane.showMessageDialog(frame, p1.getName() + " goes first.");
+                
             }
             else{
+                JOptionPane.showMessageDialog(frame, p2.getName() + " goes first.");
                 list.add(p2);
                 list.add(p1);
-                JOptionPane.showMessageDialog(frame, p2.getName() + " goes first.");
+               
             }
         }
         else if(x == JOptionPane.NO_OPTION){//3 players
@@ -154,9 +161,9 @@ public class BoardPanel extends JPanel implements MouseListener
                     showInputDialog("Player 3 enter age"));
             Player p3 = new Player(name2, a3);
             if(p1.getAge() < p2.getAge() && p1.getAge() < p3.getAge()){
-                list.add(p1);
+                
                 JOptionPane.showMessageDialog(frame, p1.getName() + " goes first.");
-
+                list.add(p1);
                 if(p2.getAge() < p3.getAge()){
                     list.add(p2);
                     list.add(p3);
@@ -168,8 +175,9 @@ public class BoardPanel extends JPanel implements MouseListener
                 }
             }
             else if(p2.getAge() < p1.getAge() && p2.getAge() < p3.getAge()){
-                list.add(p2);
+                
                 JOptionPane.showMessageDialog(frame, p2.getName() + " goes first.");
+                list.add(p2);
                 if(p3.getAge() < p1.getAge()){
                     list.add(p3);
                     list.add(p1);
@@ -181,8 +189,8 @@ public class BoardPanel extends JPanel implements MouseListener
                 }
             }      
             else{
+                JOptionPane.showMessageDialog(frame, p3.getName() + " goes first.");                
                 list.add(p3);
-                JOptionPane.showMessageDialog(frame, p3.getName() + " goes first.");
                 if(p2.getAge() < p1.getAge()){
                     list.add(p2);
                     list.add(p1);
@@ -217,8 +225,9 @@ public class BoardPanel extends JPanel implements MouseListener
 
             if(p1.getAge() < p2.getAge() && p1.getAge() < p3.getAge() && 
             p1.getAge()< p4.getAge()){
-                list.add(p1);
+                
                 JOptionPane.showMessageDialog(frame, p1.getName() + " goes first.");
+                list.add(p1);
                 if(p2.getAge() < p3.getAge() && p2.getAge() < p4.getAge()){
                     list.add(p2);
                     if(p3.getAge() < p4.getAge()){
@@ -255,8 +264,9 @@ public class BoardPanel extends JPanel implements MouseListener
             }
             else if(p2.getAge() < p1.getAge() && p2.getAge() < p3.getAge() &&
             p2.getAge() < p4.getAge()){
-                list.add(p2);
+                
                 JOptionPane.showMessageDialog(frame, p2.getName() + " goes first.");
+                list.add(p2);
                 if(p1.getAge() < p3.getAge() && p1.getAge() < p4.getAge()){
                     list.add(p1);
                     if(p3.getAge() < p4.getAge()){
@@ -293,8 +303,9 @@ public class BoardPanel extends JPanel implements MouseListener
             } 
             else if(p3.getAge() < p1.getAge() && p3.getAge() < p2.getAge() &&
             p3.getAge() < p4.getAge()){
-                list.add(p3);
+                
                 JOptionPane.showMessageDialog(frame, p3.getName() + " goes first.");
+                list.add(p3);
                 if(p2.getAge() < p1.getAge() && p2.getAge() < p4.getAge()){
                     list.add(p2);
                     if(p1.getAge() < p4.getAge()){
@@ -330,8 +341,9 @@ public class BoardPanel extends JPanel implements MouseListener
                 }
             }
             else{
-                list.add(p4);
+                
                 JOptionPane.showMessageDialog(frame, p4.getName() + " goes first.");
+                list.add(p4);
                 if(p2.getAge() < p3.getAge() && p2.getAge() < p1.getAge()){
                     list.add(p2);
                     if(p3.getAge() < p1.getAge()){
