3930785fff42c7c57357592f65fb09f19ae37fb8
rs30holl <rs30holl@siena.edu>
2019-04-23 11:20:57 -0400

    More work

--- Driver.java
+++ Driver.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 /**
  * Write a description of class Driver here.
@@ -11,59 +11,29 @@ public class Driver {
     /**
      * @param args not used
      */
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         OpeningScreen.main(new String[0]);
         boolean done = false;
         int pos = 0;
+        while (BoardPanel.list.size() < 2){
+            Thread.sleep(1000);
+        }
         while (!done){
             Player currentPlayer = BoardPanel.list.get(pos);
-            currentPlayer.turn();
+            currentPlayer.setState(true);
             if (currentPlayer.getTaxiCount() <= 2){
                 for (int i = 0; i < BoardPanel.list.size() - 1; i++){
                     pos++;
                     if (pos == BoardPanel.list.size())pos = 0;
-                    BoardPanel.list.get(pos).turn();
+                    //BoardPanel.list.get(pos).turn(0, null, null);
                 }
                 done = true;
             }
+            currentPlayer.setState(false);
             pos++;
             if (pos == BoardPanel.list.size())pos = 0;
         }
     }
 
 }
-=======
 
-/**
- * Write a description of class Driver here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Driver {
-
-    /**
-     * @param args not used
-     */
-    public static void main(String[] args){
-        OpeningScreen.main(new String[0]);
-        boolean done = false;
-        int pos = 0;
-        while (!done){
-            Player currentPlayer = BoardPanel.list.get(pos);
-            currentPlayer.turn();
-            if (currentPlayer.getTaxiCount() <= 2){
-                for (int i = 0; i < BoardPanel.list.size() - 1; i++){
-                    pos++;
-                    if (pos == BoardPanel.list.size())pos = 0;
-                    BoardPanel.list.get(pos).turn();
-                }
-                done = true;
-            }
-            pos++;
-            if (pos == BoardPanel.list.size())pos = 0;
-        }
-    }
-
-}
->>>>>>> b64d100e30e0e8d048e3d5e73471afa7f80f3043
--- Player.java
+++ Player.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -18,6 +17,7 @@ public class Player
     private int age;
     private ArrayList<Path> claimedRoutes = new ArrayList<>();
     private int taxiCount;
+    private boolean state;
 
     /**
      * Constructor for the objects of Player class
@@ -29,6 +29,7 @@ public class Player
         name = n;
         age = x;
         taxiCount = 15;
+        state = false;
     }
 
     /**
@@ -40,6 +41,13 @@ public class Player
         name = n;
     }
     
+    /**
+     * 
+     */
+    public void setState(boolean s){
+        state = s;
+    }
+
     /**
      * This method returns the age of the Player
      * 
@@ -120,158 +128,19 @@ public class Player
     /**
      * This method lets the Player make their move
      */
-    public void turn(){
-        /*
-                     if (e.get instanceof Image){//Pick up two Transportation Cards
-            drawCard(e);
-            drawCard(e);
+    public void turn(int n, Path p, Card c){
+
+        if (n == 0){//Pick up two Transportation Cards
+            drawCard(c);
+            drawCard(c);
         }
-        else if (e){//Pick up a Destination Ticket
-            drawCard(e);
+        else if (n == 1){//Pick up a Destination Ticket
+            drawCard(c);
         }
         else  {//Claim a route
-            claimRoute(e);
+            claimRoute(p);
         }
 
-         */
     }
 }
-=======
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
-import java.util.*;
-/**
- * Write a description of class Player here.
- *
- * @author Ryan Holland, Julia Krasinski, Briella Sala,
- * Matt Harrison, Michael Lostritto
- * @version 4.7.2019
- */
-public class Player
-{
-    private Hand hand;
-    private String name;
-    private int age;
-    private ArrayList<Path> claimedRoutes = new ArrayList<>();
-    private int taxiCount;
 
-    /**
-     * Constructor for the objects of Player class
-     * 
-     * @param n: Name of the Player
-     */
-    public Player(String n, int x){
-        hand = new Hand();
-        name = n;
-        age = x;
-        taxiCount = 15;
-    }
-
-    /**
-     * This method sets the name of the Player
-     * 
-     * @param n: the name of the Player
-     */
-    public void setName(String n){
-        name = n;
-    }
-    
-    /**
-     * This method returns the age of the Player
-     * 
-     * @return age: age of the Player
-     */
-    public int getAge(){
-        return age;
-    }
-
-    /**
-     * This method finds the name of the Player
-     * 
-     * @return the name of the Player
-     */
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * This method returns the taxiCount of the Player
-     *
-     * @return the taxiCount of the Player
-     */
-    public int getTaxiCount(){ return taxiCount;}
-
-    /**
-     * This method draws cards from the deck and adds them to the Players's hand
-     */
-    public void drawCard(Card c){
-        if (c instanceof TransportationCard){
-            hand.addCard(Board.tcDeck.get(0));
-            Board.tcDeck.remove(0);
-        }
-        else {
-            hand.addCard(Board.dtDeck.get(0));
-            Board.dtDeck.remove(0);
-        }
-    }
-
-    /**
-     * This method allows a Player to claim a route
-     * 
-     * @param p: a path referring to the route a player wants to claim
-     */
-    public void claimRoute(Path p){
-        int length = p.getLength();
-        String color = p.getColor();
-        //number of matching cards in hand
-        int numCards = 0;
-        for (Card c : hand.cards){
-            if (c instanceof TransportationCard){
-                //checks to see if the color of the path matches the color
-                //of the transportation card
-                if (c.getColor().equals(color)){
-                    numCards++;
-                    //adds the card being played to the discard array list
-                    Board.discard.add(c);
-                    //removes the card from the Player's hand
-                    hand.removeCard(c);
-                }
-            }
-        }
-        // if the number of matching cards is greater than or equal to the
-        // length of the path than the route can be claimed
-        if (numCards >= length){
-            //adds path to claimed routes
-            claimedRoutes.add(p);
-            //removes that path from possible paths for other Players to claim
-            p.getEnd().removePath(p);
-        }
-        else {
-            for (int i = 0; i <= numCards; i++){
-                hand.addCard(Board.discard.remove(Board.discard.size() - 1));
-            }
-        }
-    }
-
-    /**
-     * This method lets the Player make their move
-     */
-    public void turn(){
-        /*
-                     if (e.get instanceof Image){//Pick up two Transportation Cards
-            drawCard(e);
-            drawCard(e);
-        }
-        else if (e){//Pick up a Destination Ticket
-            drawCard(e);
-        }
-        else  {//Claim a route
-            claimRoute(e);
-        }
-
-         */
-    }
-}
->>>>>>> b64d100e30e0e8d048e3d5e73471afa7f80f3043
