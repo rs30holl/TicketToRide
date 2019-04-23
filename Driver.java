7b6759241a1efb6540ed8b42e5111a7447cd406d
Julia Krasinski <jl08kras@siena.edu>
2019-04-23 14:25:04 -0400

    fixed merging issue

--- Driver.java
+++ Driver.java
@@ -1,40 +1,3 @@
-
-<<<<<<< HEAD
-/**
- * Write a description of class Driver here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Driver {
-
-    /**
-     * @param args not used
-     */
-    public static void main(String[] args){
-        OpeningScreen.main(new String[0]);
-        boolean done = false;
-        int pos = 0;
-        while (!done){
-            Player currentPlayer = BoardPanel.list.get(pos);
-            currentPlayer.turn();
-            if (currentPlayer.getTaxiCount() <= 2){
-                for (int i = 0; i < BoardPanel.list.size() - 1; i++){
-                    pos++;
-                    if (pos == BoardPanel.list.size())pos = 0;
-                    BoardPanel.list.get(pos).turn();
-                }
-                done = true;
-            }
-            pos++;
-            if (pos == BoardPanel.list.size())pos = 0;
-        }
-    }
-
-}
-
-=======
-
 /**
  * Write a description of class Driver here.
  *
@@ -71,5 +34,3 @@ public class Driver {
     }
 
 }
-
->>>>>>> 3930785fff42c7c57357592f65fb09f19ae37fb8
