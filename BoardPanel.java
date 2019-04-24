420b3139dcdf119b8b56f5a8ee8942dd9e44d633
Julia Krasinski <jl08kras@siena.edu>
2019-04-23 21:04:31 -0400

    Added buttons

--- BoardPanel.java
+++ BoardPanel.java
@@ -263,6 +263,16 @@ public class BoardPanel extends JPanel implements MouseListener
                 list.add(p3);
             }
         }
+        JButton o1 = new JButton("Orange");
+        JButton o2= new JButton("Orange");
+        frame.setLayout(null);
+        //b.setBounds(100,100,140,40);
+        panel.add(o1);
+        panel.add(o2);
+        o1.setBounds(220, 30, 100, 27);
+        o2.setBounds(321, 30, 100, 27);
+        
+        
     }
 
     public void mousePressed(MouseEvent e){}
